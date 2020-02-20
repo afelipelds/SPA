e71e8010ddb2baddd521a04e760a172b6606ef0d
afelipelds <afelipelds.ez@gmail.com>
2020-02-19 22:21:10 -0500

    Final del proyecto | falta yml y deploy en gh-pages

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,23 @@
-const Character = () => {
+import getHash from '../utils/get-hash'
+import getData from '../utils/get-data'
+
+const Character = async () => {
+    const id = getHash()
+    const character = await getData(id)
+    console.log('character',character)
     const view = `
-        <div class="Characters-inner">
+        <div class="Characters-inner" id="character-${character.id}">
             <article class="Characters-card">
-                <img src="image" alt="image">
-                <h2>Name</h2>
+                <img src="${character.image}" alt="${character.image}">
+                <h2>${character.name}</h2>
             </article>
             <article className="Characters-card">
-                <h3>Episodes:</h3>
-                <h3>Status:</h3>
-                <h3>Specoes:</h3>
-                <h3>Genre:</h3>
-                <h3>Origin:</h3>  
-                <h3>Las Location:</h3>
+                <h3>Episodes: <span>${character.episode.length}</span></h3>
+                <h3>Status: <span>${character.status}</span></h3>
+                <h3>Species: <span>${character.species}</span></h3>
+                <h3>Gender: <span>${character.gender}</span></h3>
+                <h3>Origin: <span>${character.origin.name}</span></h3>  
+                <h3>Las Location: <span>${character.location.name}</span></h3>
             </article>
         </div>
     `;
